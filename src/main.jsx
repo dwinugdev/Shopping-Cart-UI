1aa1acf4c2d2e58472e669239000d5dd5b4c0703
dwiNugroho <dwinug.dev@gmail.com>
2025-08-10 22:03:46 +0700

    Add Header & Cart Count

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import './index.css';
 import App from './App.jsx';
 import { ProductProvider } from './context/ProductContext.jsx';
 import { CartProvider } from './context/CartContext.jsx';
+import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')).render(
-  <ProductProvider>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </ProductProvider>
+  <StrictMode>
+    <ProductProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductProvider>
+  </StrictMode>
 );
