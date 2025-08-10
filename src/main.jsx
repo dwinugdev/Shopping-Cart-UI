91dc8329e438aaa694d0b8b79c516d5ad8dbca39
dwiNugroho <dwinug.dev@gmail.com>
2025-08-10 16:00:37 +0700

    Add CartContext

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { ProductProvider } from './context/ProductContext.jsx';
+import { CartProvider } from './context/CartContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <ProductProvider>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </ProductProvider>
 );
